60930123dd2bd937c59f4e205c2630ba219788ee
Marta Ocana <martacyta2021@gmail.com>
2024-08-12 19:24:04 +0200

    create environment folder

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyBL02IMmk19y4jTDFkQMOiUdOUB2qjs53U",
+    authDomain: "blog-app-dashboard-6797b.firebaseapp.com",
+    projectId: "blog-app-dashboard-6797b",
+    storageBucket: "blog-app-dashboard-6797b.appspot.com",
+    messagingSenderId: "633780146277",
+    appId: "1:633780146277:web:9fa756b55eacedb19f768c"
+  }
+}
