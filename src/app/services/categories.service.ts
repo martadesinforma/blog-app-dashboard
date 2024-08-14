4bd41b2fdbff84eb767ca7b8cfec49839e4439e7
Marta Ocana <martacyta2021@gmail.com>
2024-08-14 15:09:05 +0200

    create and update categories service

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { addDoc, collection, doc, Firestore } from '@angular/fire/firestore';
+import { NgForm } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriesService {
 
-  constructor() { }
+  constructor(private firestore: Firestore) { }
+
+
+  //  saveData es una función asíncrona en Angular que se encarga de guardar datos en una colección específica de Firestore
+  async saveData(data: any) { // La función saveData ahora es async, lo que te permite manejar promesas de manera más sencilla usando await. Esto asegura que el documento se añada correctamente antes de continuar con otras acciones.
+    try {
+      const categoriesCollection = collection(this.firestore, 'categories'); // Crea/obtiene la colección 'categories' en firestore
+
+      const docRef = await addDoc(categoriesCollection, data);   // Añade el documento a la colección categories creada.
+
+
+    } catch (e) { //maneja posibles errores
+      console.error('Error adding document: ', e);
+    }
+  }
 }
