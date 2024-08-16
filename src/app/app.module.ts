5e8cb139a2edbcaf7be747244cc2b5147c6788bf
Marta Ocana <martacyta2021@gmail.com>
2024-08-16 12:36:09 +0200

    add ngx-toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,6 +28,9 @@ import { CategoriesComponent } from './categories/categories.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule, // para habilitar la funcionalidad de formularios reactivos en la aplicación. Específicamente, FormsModule permite el uso de la directiva ngModel, que se utiliza para enlazar datos bidireccionalmente entre los elementos del formulario HTML y las propiedades del componente.
+    ReactiveFormsModule,
+    ToastrModule.forRoot(), //para configurar el módulo a nivel global cuando lo importas en el módulo raíz de la aplicación. Esto significa que ngx-toastr se configurará de una vez y estará disponible para ser usado en cualquier parte de tu aplicación.
+    BrowserAnimationsModule,
 
   ],
   providers: [
