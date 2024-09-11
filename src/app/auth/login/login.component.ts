1523ebc1a21c99e9b38d324e206770bcf0d1a441
Marta Ocana <martacyta2021@gmail.com>
2024-09-11 19:32:36 +0200

    update login component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -10,6 +10,7 @@ export class LoginComponent {
 
   constructor(private authService: AuthService) {}
 
+  //el email que hay que ingresar (el que está configurado en el Authentication de Firebase y que tiene que coincidir con el ingresado por el usuario) es: marta@gmail.com y la contraseña 123456
   onSubmit(formValue:any){
     this.authService.login(formValue.email, formValue.password)
   }
