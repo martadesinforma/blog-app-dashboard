da8b3a12ee642c8e44b3afff613ebbba4c628052
Marta Ocana <martacyta2021@gmail.com>
2024-09-16 17:16:43 +0200

    update app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { AllPostComponent } from './posts/all-post/all-post.component';
 import { NewPostComponent } from './posts/new-post/new-post.component';
 import { LoginComponent } from './auth/login/login.component';
 import { authGuard } from './services/auth.guard';
+import { SubscribersComponent } from './subscribers/subscribers.component';
 
 const routes: Routes = [
   {path: '', component: DashboardComponent, canActivate: [authGuard]}, //Determina si se permite la activación de la ruta. Si el authGuard retorna `false` o un `UrlTree`, la navegación se cancela o redirige. Si authGuard retorna true, entonces si permite navegar a esta ruta.
@@ -14,6 +15,7 @@ const routes: Routes = [
 
   {path: 'posts', component: AllPostComponent, canActivate: [authGuard]},
   {path: 'posts/new', component: NewPostComponent, canActivate: [authGuard]},
+  {path: 'subscribers', component: SubscribersComponent, canActivate: [authGuard]},
 ];
 
 @NgModule({
